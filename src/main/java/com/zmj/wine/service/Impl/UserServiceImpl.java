21ab873a4e76849e53f9d78007a295c417797743
qq54511380 <5454117380@qq.com>
2018-06-06 17:18:42 +0800

    添加了查询所有会员用户的方法，同时修改了相应的类。2018/6/6 17:18

---
+++ src/main/java/com/zmj/wine/service/Impl/UserServiceImpl.java
@@ -0,0 +1,28 @@
+package com.zmj.wine.service.Impl;
+
+import com.zmj.wine.dao.UserMapper;
+import com.zmj.wine.entity.User;
+import com.zmj.wine.service.UserService;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+/**
+ * @athor hutao
+ **/
+@Service
+@Transactional
+public class UserServiceImpl implements UserService {
+
+    @Resource
+    private UserMapper userMapper;
+
+    //查询所有会员用户信息
+    @Override
+    public List<User> selectAllUser() {
+        return userMapper.selectAllUser();
+    }
+
+}
