a98000a9452eeb9c8e87f53b254be0cddae7033e
qq54511380 <5454117380@qq.com>
2018-06-08 13:00:33 +0800

    添加了查询单品的方法

--- src/main/java/com/zmj/wine/dao/ItemMapper.java
+++ src/main/java/com/zmj/wine/dao/ItemMapper.java
@@ -23,6 +23,13 @@ public interface ItemMapper {
     //后台查询所有的商品
     List<Item> selectItemAll();
 
+    //后台分页查询商品信息
+    List<Item> selectItemByPage
+    (@Param("index")int index, @Param("size")int size );
+
+    //查询商品的总数量
+    int selectItemNum();
+
     List<Item> selectByTerm(@Param("kindId") Integer kindId,@Param("subclassId") Integer subclassId,
                             @Param("brandId") Integer brandId,@Param("yieldlyId") Integer yieldlyId,
                             @Param("flavourId") Integer flavourId,@Param("proofId") Integer proofId,
