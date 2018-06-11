1059a6afa44d455af911a714be68bceb60bf9e1d
RockyYU26 <39687128+RockyYU26@users.noreply.github.com>
2018-06-11 21:37:46 +0800

    修改shoppingController baseController 2018/6/11

--- src/main/java/com/zmj/wine/controller/shoppingController.java
+++ src/main/java/com/zmj/wine/controller/shoppingController.java
@@ -1,18 +1,12 @@
 package com.zmj.wine.controller;
 
 import com.zmj.wine.entity.Item;
-import com.zmj.wine.entity.Price;
 import com.zmj.wine.entity.Shoppingcart;
 import com.zmj.wine.entity.User;
 import com.zmj.wine.service.IShoppingCartService;
 import com.zmj.wine.service.ItemService;
-import com.zmj.wine.utils.JsonResult;
-import com.zmj.wine.utils.SystemTools;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.SessionAttribute;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpSession;
@@ -27,36 +21,42 @@ public class shoppingController {
     @Resource
     private ItemService itemService;
 
+    //立即购买
     @RequestMapping("/insert")
-    public String insert(String itemName,
-                             Integer count, HttpSession httpSession){
+    public String insert(String itemName, Integer count, HttpSession httpSession){
+
         Item item = itemService.selectByName(itemName);
         Shoppingcart shoppingcart = new Shoppingcart();
-
         User currentUser = (User)httpSession.getAttribute("currentUser");
-        //如果不为空就将购物车内的商品插入数据库，如果为空酒存入cookie
-        if(currentUser!=null && currentUser.equals("")){
-            shoppingcart.setUserId(currentUser.getUserId());
-        }
-
         shoppingcart.setCartName(itemName);
         shoppingcart.setCartCount(item.getActivityPrice());
         shoppingcart.setCartDiscounts(item.getRegularPrice()-item.getActivityPrice());
         shoppingcart.setCartCount(count);
         shoppingcart.setCartImg(item.getImg1());
-
+        shoppingcart.setUserId(currentUser.getUserId());
+        //这里并没有判断是否添加成功
         int num = shoppingCartService.insert(shoppingcart);
         if(num==0){
             List<Shoppingcart> shoppingcartList = shoppingCartService.selectByUserId(shoppingcart.getUserId());
             return "shoppingCart";
+        }else{
+            return "item";
         }
-        return "item";
     }
 
-    @RequestMapping("/list")
-    public String selectByUserId(Integer userId, Model model){
-        List<Shoppingcart> shoppingcartList = shoppingCartService.selectByUserId(userId);
-        model.addAttribute("shoppingcartList",shoppingcartList);
-        return "shoppingcart";
+    //加入购物车
+    @RequestMapping("/join")
+    public String selectByUserId(String itemName, Integer count,HttpSession httpSession){
+        Item item = itemService.selectByName(itemName);
+        Shoppingcart shoppingcart = new Shoppingcart();
+        User currentUser = (User)httpSession.getAttribute("currentUser");
+        shoppingcart.setCartName(itemName);
+        shoppingcart.setCartCount(item.getActivityPrice());
+        shoppingcart.setCartDiscounts(item.getRegularPrice()-item.getActivityPrice());
+        shoppingcart.setCartCount(count);
+        shoppingcart.setCartImg(item.getImg1());
+        shoppingcart.setUserId(currentUser.getUserId());
+        shoppingCartService.insert(shoppingcart);
+        return "item";
     }
 }
