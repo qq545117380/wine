f28ef3a41e1c934e35b30ae8f9fdc767467f41c4
RockyYU26 <39687128+RockyYU26@users.noreply.github.com>
2018-06-15 09:45:54 +0800

    6/15

--- src/main/java/com/zmj/wine/service/impl/RelevanceServiceImpl.java
+++ src/main/java/com/zmj/wine/service/impl/RelevanceServiceImpl.java
@@ -4,10 +4,12 @@ import com.zmj.wine.dao.RelevanceMapper;
 import com.zmj.wine.entity.Relevance;
 import com.zmj.wine.service.IRelevanceService;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
 import java.util.List;
 @Service
+@Transactional
 public class RelevanceServiceImpl implements IRelevanceService{
     @Resource
     private RelevanceMapper relevanceDAO;
