324dcbb7508dd13f83a2194ee18943a501426e6b
Administrator <2422945224@qq.com>
2018-06-16 14:15:23 +0800

    修改微信支付代码-冯宽-2018-6-16-14：16

--- src/main/java/com/zmj/wine/controller/ImageController.java
+++ src/main/java/com/zmj/wine/controller/ImageController.java
@@ -1,33 +1,62 @@
 package com.zmj.wine.controller;
+import com.zmj.wine.utils.PayCommonUtil;
+import com.zmj.wine.utils.ZxingUtil;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 import javax.imageio.ImageIO;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.Random;
 
 /**
  * 用于获取二维码图片显示到页面上面
  * Created by jackiechan on 2018/2/2/上午11:31
  */
-@WebServlet("/payment/image")
-public class ImageController extends HttpServlet{
-    @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws
-            ServletException, IOException {
-        this.doPost(req, resp);
-    }
-    @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws
-            ServletException, IOException {
-        BufferedImage image = (BufferedImage)
-                req.getSession().getAttribute("image");
-        //获取存放的二维码
-        if (image != null) {
-            //发送到页面
-            ImageIO.write(image, "JPEG", resp.getOutputStream());
+@Controller
+@RequestMapping("/imageServlet")
+public class ImageController {
+    Random random= new Random();
+
+
+    @RequestMapping("/image")
+    public void image(HttpServletRequest req, HttpServletResponse resp){
+        String price = "1";
+//        String body = req.getParameter("body");
+        String body = "body";
+        if (req.getMethod().equalsIgnoreCase("get")) {
+            try {
+                body = new String(body.getBytes("ISO8859-1"), "UTF-8");
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            }
         }
+        String orderId = random.nextInt(100000000) + "";
+        try {
+            String url = null;
+            try {
+                url = PayCommonUtil.weixin_pay(price, body, orderId);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            BufferedImage image = ZxingUtil.createImage(url, 300, 300);//
+            req.getSession().setAttribute("oid", orderId);
+
+//        BufferedImage images = (BufferedImage)
+//                req.getSession().getAttribute("image");
+            if (image != null) {
+                try {
+                    ImageIO.write(image, "JPEG", resp.getOutputStream());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
 }
