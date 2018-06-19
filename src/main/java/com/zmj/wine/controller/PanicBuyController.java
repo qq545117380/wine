5c7391a2c9e08cea72877f10bc2a2e085d3615cf
RockyYU26 <39687128+RockyYU26@users.noreply.github.com>
2018-06-19 13:58:12 +0800

    6/19  上传特卖会的类

--- src/main/java/com/zmj/wine/controller/PanicBuyController.java
+++ src/main/java/com/zmj/wine/controller/PanicBuyController.java
@@ -1,6 +1,9 @@
 package com.zmj.wine.controller;
 
 import com.zmj.wine.entity.Item;
+import com.zmj.wine.entity.Shoppingcart;
+import com.zmj.wine.entity.User;
+import com.zmj.wine.service.IShoppingCartService;
 import com.zmj.wine.service.ItemService;
 import com.zmj.wine.utils.RedisUtil;
 import org.springframework.data.redis.core.StringRedisTemplate;
@@ -10,8 +13,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpSession;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
+//“去结算”接口
 @Controller
 @RequestMapping("/panicBuy")
 public class PanicBuyController {
@@ -21,58 +27,84 @@ public class PanicBuyController {
     @Resource
     private ItemService itemService;
 
+    @Resource
+    private IShoppingCartService shoppingCartService;
 
     @Resource
     private StringRedisTemplate stringRedisTemplate;
 
     @RequestMapping("/buy")
-    public String panicBuying(Integer count, Integer itemId, String itemName,
+    public String panicBuying( Integer itemId, String itemName,Integer count,
                               HttpSession httpSession, Model model){
-        String key="panicBuying"+String.valueOf(itemId);
+        //抢购商品的总数库存
+        String key="PBRepo"+String.valueOf(itemId);
+        //销量
+        String key2="PBSales"+String.valueOf(itemId);
         Item item=itemService.selectByName(itemName);
-        String number;
+        String repoNum;
+        String salesNum;
         try{
-            number = redisUtil.get(key).toString();
+            repoNum = redisUtil.get(key).toString();
         }catch (NullPointerException e){
             synchronized (key){
                 try{
-                    number = redisUtil.get(key).toString();
+                    repoNum = redisUtil.get(key).toString();
                 }catch (NullPointerException e2){
-                    redisUtil.set(key,String.valueOf(item.getItemPreferential()));
+                    //设置商品库存
+                    stringRedisTemplate.opsForValue().set(key,String.valueOf(item.getItemPreferential()));
+                    stringRedisTemplate.opsForValue().set(key2,"0");
+                    //设置X（库存数）个这个单品
+                    for(int i=1;i<=item.getItemPreferential();i++){
+                        stringRedisTemplate.opsForValue().set("BP"+i,"1");
+                    }
                 }
             }
         }
 
-        //开启redis事务支持（----应该需要在xml里也配置---
+        //开启redis事务支持
         stringRedisTemplate.setEnableTransactionSupport(true);
-        //查看redis内该商品的剩余数量
-        String balanceStr = stringRedisTemplate.opsForValue().get(key);
-        int balance=Integer.parseInt(balanceStr);
-        //如果剩余数量大于1
-        if(balance >= 1){
+        //商品库存
+        Integer goodsRepo = Integer.parseInt(stringRedisTemplate.opsForValue().get(key));
+        //销量数
+        Integer goodsSales = Integer.parseInt(stringRedisTemplate.opsForValue().get(key2));
+        //如果销量小于库存
+        if(goodsSales<goodsRepo){
             //监控key
             stringRedisTemplate.watch(key);
             //开启事务
             stringRedisTemplate.multi();
-            //应该要多查一次，否则exec提交始终返回空（王鹏发现的bug）
+            //将redis内的库存量减一
+            stringRedisTemplate.opsForValue().increment(key, -count);
+            //应该要多查一次，否则exec提交始终返回空（王鹏提示的bug）
             stringRedisTemplate.opsForValue().get(key);
-            long decr = redisUtil.decr(key, count);
+//            int i = Integer.parseInt(iStr);
+            //设置商品的过期时间为10分钟
+            stringRedisTemplate.expire("BP"+(goodsSales+1),10*60, TimeUnit.SECONDS);
+
             //提交事务
             List<Object> list = stringRedisTemplate.exec();
-
             if(list == null || list.size() ==0){
-                System.out.println("抢购失败");
+                model.addAttribute("msg","fail");
+                return "temaihui";
             }else{
-                itemService.updateByPreferential(
-                    item.getItemPreferential()-1, itemId);
+                Shoppingcart shoppingcart = new Shoppingcart();
+                User currentUser = (User)httpSession.getAttribute("currentUser");
+                shoppingcart.setCartName(itemName);
+                shoppingcart.setCartPrice(item.getActivityPrice());
+                shoppingcart.setCartDiscounts(item.getRegularPrice()-item.getActivityPrice());
+                shoppingcart.setCartCount(count);
+                shoppingcart.setCartImg(item.getImg1());
+                shoppingcart.setUserId(currentUser.getUserId());
+                int num = shoppingCartService.insert(shoppingcart);
+                model.addAttribute("msg","success");
+                List<Shoppingcart> shoppingcartList = shoppingCartService.selectByUserId(shoppingcart.getUserId());
+                model.addAttribute("shoppingcartList",shoppingcartList);
+                return "shoppingCart";
             }
-            return "temaihui";
         }else{
-
+            model.addAttribute("msg","fail");
+            model.addAttribute("item",item);
+            return "temaihui";
         }
-        return "temaihui";
     }
-
-
-
 }
